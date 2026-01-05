716499f7c16327a39076f1d75a2a122db5bb562c
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-01-05 12:36:57 +0600

    Update path module and os module

--- node_treat/os.js
+++ node_treat/os.js
@@ -1,34 +1,34 @@
-const os = require("os")
+const os = require("os");
 
-const osPlatform = os.platform()
-console.log("This is os platform: ", osPlatform)
+const osPlatform = os.platform();
+console.log("This is os platform: ", osPlatform);
 
-const userInfo = os.userInfo()
-console.log("User: ", userInfo)
+const userInfo = os.userInfo();
+console.log("User: ", userInfo);
 
-const osArch = os.arch()
-console.log("CPU architecture: ", osArch)
+const osArch = os.arch();
+console.log("CPU architecture: ", osArch);
 
-const osFreeMemory = os.freemem()
-console.log("Free memory: ", osFreeMemory, " bytes")
+const osFreeMemory = os.freemem();
+console.log("Free memory: ", osFreeMemory, " bytes");
 
-const totalMemory = os.totalmem()
-console.log("Total memory: ", totalMemory, " bytes")
+const totalMemory = os.totalmem();
+console.log("Total memory: ", totalMemory, " bytes");
 
-const upTime = os.uptime()
-console.log("Uptime: ", upTime, " second")
+const upTime = os.uptime();
+console.log("Uptime: ", upTime, " second");
 
-const homeDir = os.homedir()
-console.log("Home directory: ", homeDir)
+const homeDir = os.homedir();
+console.log("Home directory: ", homeDir);
 
-const hostName = os.hostname()
-console.log("Host name: ", hostName)
+const hostName = os.hostname();
+console.log("Host name: ", hostName);
 
-const networkInterfaces = os.networkInterfaces()
-console.log("Network interfaces: ", networkInterfaces)
+const networkInterfaces = os.networkInterfaces();
+console.log("Network interfaces: ", networkInterfaces);
 
-const cpus = os.cpus()
-console.log("CPUS: ", cpus)
+const cpus = os.cpus();
+console.log("CPUS: ", cpus);
 
-const tpmDir = os.tmpdir()
-console.log("Temporary directory: ", tpmDir)
+const tpmDir = os.tmpdir();
+console.log("Temporary directory: ", tpmDir);
--- node_treat/path.js
+++ node_treat/path.js
@@ -1,19 +1,21 @@
-const path = require("path")
-
+const path = require("path");
 
 // console.log(__dirname)
 // console.log(__filename)
 
-const filePath = path.join("New Folder", "Backend Development", "Practice", "Node.js")
+const filePath = path.join(
+  "New Folder",
+  "Backend Development",
+  "Practice",
+  "Node.js"
+);
 
 // console.log(filePath)
 
-const parseFile = path.parse(filePath)
-const baseName = path.basename(filePath)
-const dirName = path.dirname(filePath)
-const extName = path.extname(filePath)
-const resolveFile = path.resolve(filePath)
+const parseFile = path.parse(filePath);
+const baseName = path.basename(filePath);
+const dirName = path.dirname(filePath);
+const extName = path.extname(filePath);
+const resolveFile = path.resolve(filePath);
 
-
-
-console.log({parseFile, baseName, dirName, extName, resolveFile})
+console.log({ parseFile, baseName, dirName, extName, resolveFile });
