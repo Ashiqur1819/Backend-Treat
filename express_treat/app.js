095763376000c4fe643aa2d101ec758f26ab54bf
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-01-21 12:14:48 +0600

    Validate environment variables using zod

--- express_treat/app.js
+++ express_treat/app.js
@@ -1,11 +1,11 @@
-import express from "express"
-const app = express()
-const PORT = 3000
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hello World</h2>")
-})
+  res.send("<h2>Hello World</h2>");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running successfully on port: ${PORT}`)
-})
+  console.log(`Server is running successfully on port: ${PORT}`);
+});
